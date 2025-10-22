676b2212b7ba955cf8510175993ce2675682be4e
Jean Barbosa <jeanbarbosa.it@gmail.com>
2025-10-21 21:13:53 -0300

    Implementação da User controller na camada API.

--- TaskPilot/TaskPilot.API/Controllers/UsersController.cs
+++ TaskPilot/TaskPilot.API/Controllers/UsersController.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿// TaskPilot.API/Controllers/UsersController.cs (Sem MediatR)
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using TaskPilot.Application.Features.Users.Queries.GetUserById;
+using TaskPilot.Application.Interfaces; // Novo using
 
-namespace TaskPilot.API.Controllers
+public class UsersController : ControllerBase
 {
-    public class UsersController
+    // ✅ Injeta o Serviço de Aplicação
+    private readonly IUserService _userService;
+
+    public UsersController(IUserService userService)
     {
+        _userService = userService;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get([FromRoute] int id)
+    {
+        var query = new GetUserByIdQuery { UserId = id };
+
+        var result = await _userService.GetUserByIdAsync(query);
+
+        if (result == null) return NotFound();
+        return Ok(result);
     }
 }
--- TaskPilot/TaskPilot.Application/DependencyInjection.cs
+++ TaskPilot/TaskPilot.Application/DependencyInjection.cs
@@ -4,6 +4,9 @@ using TaskPilot.Application.Features.Tasks.Commands.CreateTask;
 using TaskPilot.Application.Features.Tasks.Commands.CompleteTask;
 using TaskPilot.Application.Features.Tasks.Queries.GetTaskSummary;
 using System.Reflection;
+using MediatR;
+using TaskPilot.Application.Features.Users.Queries.GetUserById;
+using TaskPilot.Application.Interfaces;
 
 namespace TaskPilot.Application
 {
@@ -16,10 +19,36 @@ namespace TaskPilot.Application
             services.AddScoped<CompleteTaskCommandHandler>();
             services.AddScoped<GetTaskSummaryQueryHandler>();
 
+            //Adicionado para teste.
+         //   services.AddScoped<IUserService, UserService>();
+
             // Opcional: Adicionar AutoMapper se estiver usando mapeamento complexo
-            // services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            //services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            // services.AddMediatR(typeof(GetUserByIdQuery).Assembly);
+            // services.AddMediatR(typeof(GetUserByIdQuery).Assembly);
 
             return services;
         }
+
+        //public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        //{
+
+        //    // Opção 2: Mais robusta, usando uma classe conhecida na Application:
+        //    services.AddMediatR(typeof(GetUserByIdQuery).Assembly);
+              
+        //    // IMPORTANTE: Use a versão 9.0.0
+        //    // Se você não conseguir instalar o MediatR, pode ser necessário 
+        //    // usar o código da versão 12.x+ (que usa o novo AddMediatR)
+
+        //    // Se o seu projeto .NET Core 3.1 só funciona com a versão 9.0.0 do MediatR,
+        //    // a sintaxe é:
+        //    // services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetUserByIdQuery).Assembly));
+        //    // Mas para a versão 9.0.0 (antiga), o ideal é usar:
+        //    services.AddMediatR(typeof(GetUserByIdQuery).Assembly);
+
+
+        //    // ... Outros registros (AutoMapper, etc.) se aplicável
+        //    return services;
+        //}
     }
 }
