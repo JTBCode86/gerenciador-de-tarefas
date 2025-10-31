0fbd0bd881db6af0249f85f2bc4f56e8e5fddea1
Jean Barbosa <jeanbarbosa.it@gmail.com>
2025-10-31 20:26:00 -0300

    Implementando interação via Python.

--- src/TaskPilot/TaskPilot.API/Controllers/UsersController.cs
+++ src/TaskPilot/TaskPilot.API/Controllers/UsersController.cs
@@ -7,21 +7,21 @@ using TaskPilot.Application.Interfaces; // Novo using
 public class UsersController : ControllerBase
 {
     // ✅ Injeta o Serviço de Aplicação
-    private readonly IUserService _userService;
+    //private readonly IUserService _userService;
 
-    public UsersController(IUserService userService)
-    {
-        _userService = userService;
-    }
+    //public UsersController(IUserService userService)
+    //{
+    //    _userService = userService;
+    //}
 
-    [HttpGet("{id}")]
-    public async Task<IActionResult> Get([FromRoute] int id)
-    {
-        var query = new GetUserByIdQuery { UserId = id };
+    //[HttpGet("{id}")]
+    //public async Task<IActionResult> Get([FromRoute] int id)
+    //{
+    //    var query = new GetUserByIdQuery { UserId = id };
 
-        var result = await _userService.GetUserByIdAsync(query);
+    //    var result = await _userService.GetUserByIdAsync(query);
 
-        if (result == null) return NotFound();
-        return Ok(result);
-    }
+    //    if (result == null) return NotFound();
+    //    return Ok(result);
+    //}
 }
